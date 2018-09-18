88aab61d7c0771a661cf726a370737c18164a766
samussiah <samussiah@gmail.com>
2018-09-18 12:12:40 -0400

    add info icons to legend items

--- src/init/addLegend/update.js
+++ src/init/addLegend/update.js
@@ -1,14 +1,16 @@
 export default function update() {
-    this.containers.legendItems.text(
-        d =>
-            `${d[1]} (${
-                this.data.filtered.length
-                    ? d3.format('%')(
-                          this.data.filtered.filter(
-                              di => di[this.settings.rendererSynced.visit_status_col] === d[1]
-                          ).length / this.data.filtered.length
-                      )
-                    : 'N/A'
-            })`
-    );
+    this.containers.legendItems
+        .select('.pvl-legend-item-label')
+        .text(
+            d =>
+                `${d[1]} (${
+                    this.data.filtered.length
+                        ? d3.format('%')(
+                              this.data.filtered.filter(
+                                  di => di[this.settings.rendererSynced.visit_status_col] === d[1]
+                              ).length / this.data.filtered.length
+                          )
+                        : 'N/A'
+                })`
+        );
 }

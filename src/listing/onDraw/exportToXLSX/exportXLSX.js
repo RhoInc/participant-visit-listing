28f783d3e00db9dc9873cb3b8f499d1508e1d2b1
Spencer Childress <samussiah@gmail.com>
2019-09-03 17:18:38 -0400

    reformat

--- src/listing/onDraw/exportToXLSX/exportXLSX.js
+++ src/listing/onDraw/exportToXLSX/exportXLSX.js
@@ -4,21 +4,15 @@ import s2ab from './exportXLSX/s2ab';
 
 export default function exportXLSX() {
     if (!this.pvl.test) {
-        const fileName = `participant-visit-listing-${time.format('%Y-%m-%dT%H-%M-%S')(new Date())}.xlsx`;
+        const fileName = `participant-visit-listing-${time.format('%Y-%m-%dT%H-%M-%S')(
+            new Date()
+        )}.xlsx`;
         console.log(fileName);
         try {
-            const blob = new Blob(
-                [
-                    s2ab(this.XLSX)
-                ],
-                {
-                    type: 'application/octet-stream'
-                }
-            );
-            FileSaver(window)(
-                blob,
-                fileName
-            );
+            const blob = new Blob([s2ab(this.XLSX)], {
+                type: 'application/octet-stream'
+            });
+            FileSaver(window)(blob, fileName);
         } catch (error) {
             if (typeof console !== 'undefined') console.log(error);
         }
